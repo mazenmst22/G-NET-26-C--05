550d664c8157dbc7ad8e49007b016a317efbc6ae
Mazen Thabet <MazenThabet2@outlook.com>
2026-05-22 21:27:49 +0300

    Part 1

---
+++ G-NET-26-C#-05/DaysOfWeek.cs
@@ -0,0 +1,14 @@
+﻿namespace G_NET_26_CSharp_05
+{
+    public enum DaysOfWeek
+    {
+
+        Saturday = 1,
+        Sunday = 2,
+        Monday = 3,
+        Tuesday = 4,
+        Wednesday = 5,
+        Thursday = 6,
+        Friday = 7
+    }
+}
--- G-NET-26-C#-05/Program.cs
+++ G-NET-26-C#-05/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace G_NET_26_CSharp_05
+﻿
+namespace G_NET_26_CSharp_05
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
+            #region Part 1
+            Console.Write("Enter a day number (1-7): ");
+            if (int.TryParse(Console.ReadLine(), out int dayNumber))
+            {
+                DaysOfWeek selectedDay = (DaysOfWeek)dayNumber;
+
+                Console.WriteLine($"Day: {selectedDay}");
+
+                switch (selectedDay)
+                {
+                    case DaysOfWeek.Friday:
+                    case DaysOfWeek.Saturday:
+                        Console.WriteLine("It's the Weekend");
+                        break;
+                    case DaysOfWeek.Sunday:
+                    case DaysOfWeek.Monday:
+                    case DaysOfWeek.Tuesday:
+                    case DaysOfWeek.Wednesday:
+                    case DaysOfWeek.Thursday:
+                        Console.WriteLine("It's a Workday");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid day range.");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.");
+                }
+            #endregion
         }
     }
 }
